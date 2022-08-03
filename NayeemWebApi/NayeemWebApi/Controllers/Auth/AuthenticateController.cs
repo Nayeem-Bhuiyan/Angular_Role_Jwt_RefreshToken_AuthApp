8f4b15bb3dbd449023c65788fb7b0d845e28ff99
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-08-03 10:43:14 +0600

    dfgasdg

---
+++ NayeemWebApi/NayeemWebApi/Controllers/Auth/AuthenticateController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NayeemWebApi.Controllers.Auth
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+    }
+}
